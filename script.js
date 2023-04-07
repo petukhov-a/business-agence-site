bbfbfa31ef14837e462ced2872c9a8b7c599b948
Anton <reopik@mail.ru>
2023-04-07 22:50:27 +0300

    made video controls work

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('change');
 }); 
 // End of Navber
+
+//Section 2 Video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+const playPause = () => {
+    if (video.paused) {
+        btn.className = 'far fa-pause-circle';
+        video.play();
+        video.style.opacity = '0.7';
+    } else { 
+        btn.className = 'far fa-play-circle';
+        video.pause();
+        video.style.opacity = '0.3';
+    }
+}
+
+btn.addEventListener('click', () => {
+    playPause();
+});
+
+video.addEventListener('timeupdate', () => {
+    const barWidth = video.currentTime / video.duration
+    bar.style.width = `${barWidth * 100}%`;
+    if (video.ended) {
+        btn.className = 'far fa-play-circle';
+        video.style.opacity = '0.3';
+    }
+})
+//End of Section 2 Video 
